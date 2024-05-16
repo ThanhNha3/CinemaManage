9a527b5022ff8045723f4a3b653e3b8c2267fd53
ThanhNha3 <nhantpc05188@fpt.edu.vn>
2024-05-16 13:10:23 +0700

    [16/5/2024] commit code asm1

--- src/app/pages/revenue/addRevenue/addRevenue.module.ts
+++ src/app/pages/revenue/addRevenue/addRevenue.module.ts
@@ -1,6 +1,7 @@
 import { AddRevenueComponent } from './addRevenue.component';
 import { NgModule } from '@angular/core';
 import { BreadcrumbModule } from 'xng-breadcrumb';
+
 @NgModule({
   imports: [BreadcrumbModule],
   declarations: [AddRevenueComponent],
--- src/app/pages/revenue/revenue.module.ts
+++ src/app/pages/revenue/revenue.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import {RevenueComponent} from './revenue.component';
-import {BreadcrumbModule} from "xng-breadcrumb";
+import { RevenueComponent } from './revenue.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
+import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
 @NgModule({
-  imports: [
-    BreadcrumbModule
-  ],
-  declarations: [
-    RevenueComponent
-  ],
+  imports: [BreadcrumbModule,RouterModule,RouterLink],
+  declarations: [RevenueComponent],
 })
-export class RevenueModule { }
+export class RevenueModule {}
